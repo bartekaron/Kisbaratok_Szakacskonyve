b77d963f27dabdb4e472524ca2acbb4410a21fd2
UtasiB <utasibalazs@turr.hu>
2024-09-17 11:45:56 +0200

    adatbázis jax

--- szakacskonyv.sql
+++ szakacskonyv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Sze 17. 10:24
+-- Létrehozás ideje: 2024. Sze 17. 11:41
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -24,12 +24,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `categoies`
+-- Tábla szerkezet ehhez a táblához `categories`
 --
 
-CREATE TABLE `categoies` (
+CREATE TABLE IF NOT EXISTS `categories` (
   `ID` varchar(40) NOT NULL,
-  `name` varchar(100) NOT NULL
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 -- --------------------------------------------------------
@@ -38,15 +39,18 @@ CREATE TABLE `categoies` (
 -- Tábla szerkezet ehhez a táblához `recipes`
 --
 
-CREATE TABLE `recipes` (
+CREATE TABLE IF NOT EXISTS `recipes` (
   `ID` varchar(40) NOT NULL,
   `catID` varchar(40) NOT NULL,
   `userID` varchar(40) NOT NULL,
   `title` varchar(40) NOT NULL,
   `descp` varchar(200) NOT NULL,
   `time` int(30) NOT NULL,
-  `addtions` varchar(40) NOT NULL,
-  `claory` int(35) NOT NULL
+  `additions` varchar(40) NOT NULL,
+  `claory` int(35) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `catID` (`catID`),
+  KEY `userID` (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 -- --------------------------------------------------------
@@ -55,39 +59,16 @@ CREATE TABLE `recipes` (
 -- Tábla szerkezet ehhez a táblához `users`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `ID` varchar(40) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(40) NOT NULL,
   `passwd` varchar(40) NOT NULL,
   `phone` varchar(40) NOT NULL,
   `role` varchar(40) NOT NULL,
-  `status` varchar(40) NOT NULL
+  `status` varchar(40) NOT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
---
--- Indexek a kiírt táblákhoz
---
-
---
--- A tábla indexei `categoies`
---
-ALTER TABLE `categoies`
-  ADD PRIMARY KEY (`ID`);
-
---
--- A tábla indexei `recipes`
---
-ALTER TABLE `recipes`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `catID` (`catID`),
-  ADD KEY `userID` (`userID`);
-
---
--- A tábla indexei `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`ID`);
 
 --
 -- Megkötések a kiírt táblákhoz
@@ -98,7 +79,7 @@ ALTER TABLE `users`
 --
 ALTER TABLE `recipes`
   ADD CONSTRAINT `recipes_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `recipes_ibfk_2` FOREIGN KEY (`catID`) REFERENCES `categoies` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `recipes_ibfk_2` FOREIGN KEY (`catID`) REFERENCES `categories` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
