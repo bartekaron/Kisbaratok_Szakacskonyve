6e5f8ef81c84accd19c726d18161484bf0ca819f
UtasiB <80397148+UtasiB@users.noreply.github.com>
2024-09-17 10:25:36 +0200

    adatbázis alapjának elkészítése

---
+++ szakacskonyv.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2024. Sze 17. 10:24
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `szakacskonyv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `categoies`
+--
+
+CREATE TABLE `categoies` (
+  `ID` varchar(40) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `recipes`
+--
+
+CREATE TABLE `recipes` (
+  `ID` varchar(40) NOT NULL,
+  `catID` varchar(40) NOT NULL,
+  `userID` varchar(40) NOT NULL,
+  `title` varchar(40) NOT NULL,
+  `descp` varchar(200) NOT NULL,
+  `time` int(30) NOT NULL,
+  `addtions` varchar(40) NOT NULL,
+  `claory` int(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `ID` varchar(40) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `passwd` varchar(40) NOT NULL,
+  `phone` varchar(40) NOT NULL,
+  `role` varchar(40) NOT NULL,
+  `status` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `categoies`
+--
+ALTER TABLE `categoies`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- A tábla indexei `recipes`
+--
+ALTER TABLE `recipes`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `catID` (`catID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `recipes`
+--
+ALTER TABLE `recipes`
+  ADD CONSTRAINT `recipes_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `recipes_ibfk_2` FOREIGN KEY (`catID`) REFERENCES `categoies` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
