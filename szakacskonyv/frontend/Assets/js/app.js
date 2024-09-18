ca6eb06a90719a6504af6aa119b2b6f554535ea0
UtasiB <utasibalazs@turr.hu>
2024-09-18 09:02:40 +0200

    View létrehozása, bejelntkezés regisztráció fornt

---
+++ szakacskonyv/frontend/Assets/js/app.js
@@ -0,0 +1,15 @@
+const serverUrl = 'http://localhost:3000';
+let loggedUser = null;
+
+async function render(view){
+    let main = document.querySelector('main');
+    main.innerHTML = await (await fetch(`Views/${view}.html`)).text();
+    
+}
+
+if (localStorage.getItem('receptEmber')){
+    loggedUser = JSON.parse(localStorage.getItem('receptEmber'));
+    render('recipie');
+}else{
+    render('login');
+}
--- szakacskonyv/frontend/Controllers/userCtrl.js
+++ szakacskonyv/frontend/Controllers/userCtrl.js
@@ -1,14 +1,34 @@
-const serverUrl = 'http://localhost:3000';
 
-async function render(view){
-    let main = document.querySelector('main');
-    main.innerHTML = await (await fetch(`Views/${view}.html`)).text();
-    
+function regisztracio(){
+    let newUser = {
+        name: document.querySelector('#name').value,
+        email: document.querySelector('#email').value,
+        phone: document.querySelector('#phone').value,
+        passwd: document.querySelector('#passwd').value,
+        confirm: document.querySelector('#confirm').value
+    }
+
+    axios.post(`${serverUrl}/reg`, newUser).then(res => {
+        alert(res.data);
+    });
 }
 
-if (localStorage.getItem('receptEmber')){
-    loggedUser = JSON.parse(localStorage.getItem('receptEmber'));
-    render('recipie');
-}else{
-    render('login');
+function login(){
+    let user = {
+        email: document.querySelector('#email').value,
+        passwd: document.querySelector('#passwd').value
+    }
+
+    axios.post(`${serverUrl}/log`, user).then(res =>{
+       
+        if (res.status != 202){
+            alert(res.data);
+            return;
+        }
+
+        loggedUser = res.data;
+        localStorage.setItem('receptEmber', JSON.stringify(loggedUser));
+        renderNavItems();
+        render('recipes');
+    });
 }
